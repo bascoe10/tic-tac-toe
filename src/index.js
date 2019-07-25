8c17afe4ae1b599f1bdec8349f6f2e71f7183504
Sesay, Abass <bassco10@gmail.com>
2019-07-25 11:00:18 -0400

    can display a list of history of moves

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ function Square(props) {
     }
   
     render() {
-  
       return (
         <div>
           <div className="board-row">
@@ -73,6 +72,15 @@ function Square(props) {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ? `Go to move #${move}` : 'Go to game start';
+            return(
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if(winner){
             status = 'Winner: ' + winner;
@@ -90,7 +98,7 @@ function Square(props) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
